3c797fc57bf90d80693c31d5e8ba3af708701ce5
change <eduChange.hphk@gmail.com>
2019-10-22 17:14:48 +0900

    like

--- posts/models.py
+++ posts/models.py
@@ -4,4 +4,5 @@ from django.conf import settings
 class Post(models.Model):
     title = models.CharField(max_length=100) 
     content = models.TextField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_posts")
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Post
 # Create your views here.
@@ -23,3 +23,15 @@ def create(request):
         'form': form
     }
     return render(request, 'posts/form.html', context)
+
+def like(request, id):
+    post = get_object_or_404(Post, id=id)
+    user = request.user
+
+    # if user in post.like_users.all():
+    if post.like_users.filter(id=user.id):
+        post.like_users.remove(user)
+    else:
+        post.like_users.add(user)
+
+    return redirect('posts:index')
