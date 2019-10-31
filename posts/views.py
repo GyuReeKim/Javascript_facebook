0c6c71af9cfce48383bb42b05d07e1161116e2bd
GyuReeKim <starkim96@naver.com>
2019-10-31 18:06:57 +0900

    191031

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Post
+from django.http import JsonResponse
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
@@ -25,13 +26,20 @@ def create(request):
     return render(request, 'posts/form.html', context)
 
 def like(request, id):
-    post = get_object_or_404(Post, id=id)
-    user = request.user
-
-    # if user in post.like_users.all():
-    if post.like_users.filter(id=user.id):
-        post.like_users.remove(user)
+    if request.is_ajax():
+        post = get_object_or_404(Post, id=id)
+        user = request.user
+        # if user in post.like_users.all():
+        if post.like_users.filter(id=user.id):
+            post.like_users.remove(user)
+            # 좋아요 버튼이 눌렸는지 확인하는 변수
+            is_like = False
+        else:
+            post.like_users.add(user)
+            is_like = True
+        likes_count = post.like_users.all().count()
+        context = {'is_like': is_like, 'likes_count': likes_count}
+        return JsonResponse(context)
+        # return redirect('posts:index')
     else:
-        post.like_users.add(user)
-
-    return redirect('posts:index')
+        return JsonResponse({'message': '잘못된 요청입니다.'})
